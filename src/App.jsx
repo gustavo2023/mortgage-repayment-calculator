2cdd5bb2b6988700c45541bc875cfa55eab23f23
gustavo2023 <gustavoegutierrezg23@gmail.com>
2025-12-16 22:27:11 -0400

    feat: add FormInputGroup and RadioGroup components for user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,85 @@
-import { useState } from 'react'
-import './App.css'
+import classNames from "classnames";
+import "./App.css";
 
-function App() {
+function FormInputGroup({
+  id,
+  label,
+  value,
+  onChange,
+  error,
+  symbol,
+  symbolPosition = "left",
+}) {
+  const wrapperClass = classNames("form-group__control", {
+    "form-group__control--error": error,
+    "form-group__control--reverse": symbolPosition === "right",
+  });
+
+  const symbolClass = classNames("form-group__symbol", {
+    "form-group__symbol--error": error,
+  });
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="form-group">
+      <label htmlFor={id} className="form-group__label">
+        {label}
+      </label>
+
+      <div className={wrapperClass}>
+        <span className={symbolClass}>{symbol}</span>
+        <input
+          type="number"
+          id={id}
+          value={value}
+          onChange={onChange}
+          className="form-group__input"
+        />
+      </div>
+
+      {error && <span className="form-group__error-msg">{error}</span>}
+    </div>
+  );
 }
 
-export default App
+function RadioGroup({ label, selectedValue, onChange, error }) {
+  const options = [
+    { label: "Repayment", value: "repayment" },
+    { label: "Interest Only", value: "interest-only" },
+  ];
+
+  return (
+    <div className="radio-group">
+      <span className="radio-group__label">{label}</span>
+
+      <div className="radio-group__options">
+        {options.map((option) => {
+          const optionClass = classNames("radio-option", {
+            "radio-option--selected": selectedValue === option.value,
+          });
+
+          return (
+            <label key={option.value} className={optionClass}>
+              <input
+                type="radio"
+                name="mortgagetype"
+                value={option.value}
+                checked={selectedValue === option.value}
+                onChange={(e) => onChange(e.target.value)}
+                className="radio-option__input"
+              />
+              <span className="radio-option__text">{option.label}</span>
+            </label>
+          );
+        })}
+      </div>
+
+      {error && <span className="radio-group__error-msg">{error}</span>}
+    </div>
+  );
+}
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
